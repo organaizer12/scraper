25c20ad77ae68b9fc8a8060103c1989444683aef
Sasha Penkov <organaizer12@ya.ru>
2017-11-29 09:46:10 +0300

    added categories

--- scraper.js
+++ scraper.js
@@ -9,21 +9,35 @@ var results = [];
 
 var urlList = [];
 
-needle.get(URL, function(err, res){
+
+needle.get(URL, function(err, res){ 
 	if (err) throw err;
 	var $ = cheerio.load(res.body);
 
-	$('#content>a[href]').each(function(){
-		var ref = String($(this).attr('href'));
+	$('#content>a[href]').each(function(){ //get links from the start page
+		var ref = String($(this).attr('href')); 
 		needle.get(resolve(URL, ref), function(err, res){
 		if (err) throw err;
 		var $ = cheerio.load(res.body);
 		var quantity = 0;
-		if ($('td>p').eq(0).text().trim()==='первые сутки'){
+		if ($('td>p').eq(0).text().trim()==='первые сутки'){ //chek time span
 			quantity = 1;
 		}else{
 			quantity = 2;
 		};
+		var tag_ids = [
+			"1965"];
+		($('#center>h1:contains("Палат")').text())?tag_ids.push("1967"): //chek category for tag_ids
+		($('#center>h1:contains("палат")').text())?tag_ids.push("1967"):
+		($('#center>h1:contains("мешок")').text())?tag_ids.push("1968"):
+		($('#center>h1:contains("коврик")').text())?tag_ids.push("1969"):
+		($('#center>h1:contains("пенка")').text())?tag_ids.push("1969"):
+		($('#center>h1:contains("Тент")').text())?tag_ids.push("1967"):
+		($('#center>h1:contains("тент")').text())?tag_ids.push("1967"):
+		($('#center>h1:contains("Рюкзак")').text())?tag_ids.push("10313"):
+		($('#center>h1:contains("рюкзак")').text())?tag_ids.push("10313"):
+		tag_ids.push("1998");
+		
 		results.push({
 			"name": $('#center>h1').text().trim(),
 		    "descr": $('#center>p').eq(0).text().trim(),
@@ -57,14 +71,12 @@ needle.get(URL, function(err, res){
 		      "lat": "59.830452",
 		      "lng": "30.370978"
 		    },
-		    "tag_ids": [
-		      "1965",
-		      "1967"
-		    ],
+		    "tag_ids": tag_ids,
 		    "url": resolve(URL, ref)
 
 			});
 			fs.writeFileSync('./data.json', JSON.stringify(results, null, 2));
+			tag_ids = []
 	});
 
 	});
